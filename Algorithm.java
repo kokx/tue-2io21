7a07154750e1bbe1780ac9a9b40d11f1ce9f58b0
Pieter Kokx <pieter@kokx.nl>
2012-05-07 15:55:44 +0200

    Changes to the algorithm class.

--- Algorithm.java
+++ Algorithm.java
@@ -1,22 +1,41 @@
-public abstract class Algorithm {
+public abstract class Algorithm
+{
+
+    protected Field field;
+
+    /**
+     * Constructor.
+     *
+     * @param field
+     */
+    public Algorithm(Field field)
+    {
+        this.field = field;
+    }
+
+    /**
+     * Run the algorithm.
+     */
+    public abstract void run()
     
     /**
+     * Determine the distance between sourcePoint and destPoint.
      * 
-     * @param Point sourcePoint
-     * @param Point destPoint
-     * @param type; 2 = Euclidean, 1 = Manhatten
+     * @param sourcePoint Source point
+     * @param destPoint Destination point
+     * @param m 2 = Euclidean, 1 = Manhatten
      * @return Distance from sourcePoint to destPoint
      */
-    int Distance(Point sourcePoint, Point destPoint, int m) {
+    public int Distance(Point sourcePoint, Point destPoint, int m) {
         switch (m) {
-        case 1: 
-            return Math.abs(destPoint.getX() - sourcePoint.getX()) 
+            case 1: 
+                return Math.abs(destPoint.getX() - sourcePoint.getX()) 
                     + Math.abs(destPoint.getY() - sourcePoint.getY());
-            break;
-        case 2:
-        return Math.sqrt(Math.pow(Math.abs(destPoint.getX() - sourcePoint.getX()), 2) 
-                + Math.pow(Math.abs(destPoint.getY() - sourcePoint.getY()), 2));
-        break;
+                break;
+            case 2:
+                return Math.sqrt(Math.pow(Math.abs(destPoint.getX() - sourcePoint.getX()), 2) 
+                        + Math.pow(Math.abs(destPoint.getY() - sourcePoint.getY()), 2));
+                break;
         }
     }
 
